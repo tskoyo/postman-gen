b2e2642d7144a8d37129c84899fd211030b714c8
tskoyo <jovansr@protonmail.com>
2025-11-02 00:21:15 +0100

    First macro code

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
 
+#[proc_macro_derive(Payload, attributes(endpoint, description, field))]
+pub fn derive_payload(input: TokenStream) -> TokenStream {
+    let derive_input = parse_macro_input!(input as DeriveInput);
+    println!("Deriving Payload for: {}", derive_input.ident);
+
+    TokenStream::new()
+}
